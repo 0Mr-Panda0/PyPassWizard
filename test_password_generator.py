7372377f49f19e667f4a9e2ec398504a7ecc4775
Karan Behera <42995800+0Mr-Panda0@users.noreply.github.com>
2024-06-17 20:57:19 +0530

    Update test_password_generator.py

--- test_password_generator.py
+++ test_password_generator.py
@@ -7,6 +7,6 @@ def test_password_generator():
     case_1 = creating_password(12, "Yes")
     case_2 = creating_password(7, "No")
     assert len(case_1) == 12
-    assert len(case_2) == 8 or punctuation not in case_2
+    assert len(case_2) == 7 and punctuation not in case_2
     case_3 = storing_password(case_1)
     assert os.path.isfile("password.txt")
